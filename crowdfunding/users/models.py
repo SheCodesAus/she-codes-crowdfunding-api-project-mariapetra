a5f4503f57bf00281142366b47689725db04e0e6
Mara Petra <62734302+mariapetra@users.noreply.github.com>
2022-04-18 08:29:57 +0800

    moved procfile

--- crowdfunding/users/models.py
+++ crowdfunding/users/models.py
@@ -2,7 +2,7 @@ from django.contrib.auth.models import AbstractUser
 from django.db import models
 
 class CustomUser(AbstractUser):
-    profile_image=models.URLField(null=True, blank=True)
+    # profile_image=models.URLField(null=True, blank=True)
 
 
     def __str__(self):
