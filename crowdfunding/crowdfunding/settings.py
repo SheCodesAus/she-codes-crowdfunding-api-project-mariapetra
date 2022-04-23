279283e8db5e96fe22a61a19108ae6a8e555acef
Mara Petra <62734302+mariapetra@users.noreply.github.com>
2022-04-23 11:41:47 +0800

    updated settings to enable login via eb

--- crowdfunding/crowdfunding/settings.py
+++ crowdfunding/crowdfunding/settings.py
@@ -53,8 +53,9 @@ INSTALLED_APPS = [
 ]
 
 REST_FRAMEWORK = {
-    'DEFAULT_AUTHENTICATION_CLASSES': [
-    'rest_framework.authentication.TokenAuthentication',
+    "DEFAULT_AUTHENTICATION_CLASSES": [
+        "rest_framework.authentication.TokenAuthentication",
+        'rest_framework.authentication.SessionAuthentication',
     ]
 }
 
